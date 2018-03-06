f43dc53e545826d270890efe87c6d66527dd4fc9
Stechmuck <stechmuck@brettspielwelt.de>
2018-03-06 11:34:10 +0100

    Added exports for external access

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,9 @@
  * Time: 23:37
  */
 module com.clevertap.apns {
+	exports com.clevertap.apns;
+	exports com.clevertap.apns.clients;
+	
     requires jdk.incubator.httpclient;
     requires jackson.core;
     requires jackson.databind;
