0e227c8393f5416b51f47deab32e1d4cbf93d78a
Mathäus Zingerle <mathaeus.zingerle@willhaben.at>
2018-11-21 12:06:09 +0100

    Added method to add thread-id to aps, see https://developer.apple.com/library/archive/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/PayloadKeyReference.html, in order to support the new iOS 12 feature to group notifications on the iOS device

--- src/main/java/com/clevertap/apns/Notification.java
+++ src/main/java/com/clevertap/apns/Notification.java
@@ -223,6 +223,11 @@ public class Notification {
             return this;
         }
 
+        public Builder threadId(String threadId) {
+            aps.put("thread-id", threadId);
+            return this;
+        }
+
         public Builder customField(String key, Object value) {
             root.put(key, value);
             return this;
